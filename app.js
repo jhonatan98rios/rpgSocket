d03615f25e61cbb227745cae16c052dfa21bd46f
Jhonatan Teixeira Rios <jhonatan98rios@gmail.com>
2020-12-16 01:53:48 -0300

    port fix

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const server = require('http').createServer(app)
 const io = require('socket.io')(server)
 
 // Create the server
-server.listen(3000, () => {
+server.listen(process.env.PORT || 3000, () => {
     console.log('Running')
 })
 
