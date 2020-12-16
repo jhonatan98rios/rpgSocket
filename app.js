a874718afad245e0f6cb02c1a4cf5a96f80ff4db
Jhonatan Teixeira Rios <jhonatan98rios@gmail.com>
2020-12-16 01:43:15 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const app = express()
+const server = require('http').createServer(app)
+const io = require('socket.io')(server)
+
+// Create the server
+server.listen(3000, () => {
+    console.log('Running')
+})
+
+// Use static template like response
+app.use(express.static(__dirname + "/public"))
+
+// Connect the user with the server
+io.on('connection', async (socket) => {
+
+    // Start the connection
+    console.log('\nNova conexão')
+
+    // Exemple of message
+    socket.on('attack', () => {
+        socket.to(socket.room).emit('damage')
+    })
+
+    socket.on('logout', () => {
+        socket.to(socket.room).emit('win')
+        socket.disconnect()
+    })
+
+    // Send message for all user, except for the sender
+    socket.broadcast.emit('bc', socket.id + ' connected')
+
+    let ids = await io.allSockets();
+    let connectedUsers = [...ids]
+
+    if(connectedUsers.length <= 2){
+        socket.join('beta')
+        socket.room = 'beta'
+        console.log('beta')
+
+    }else {
+        socket.emit('failToEnter')
+        socket.disconnect()
+    }
+
+})
